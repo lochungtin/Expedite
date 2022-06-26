d482aa239dd8ead6c815c72f012cef9f354525e1
lochungtin <lochungtin@gmail.com>
2022-06-26 21:06:41 +0100

    temp

--- Nonogram/game.h
+++ Nonogram/game.h
@@ -8,8 +8,6 @@
 #include <vector>
 
 #include "board.h"
-#include "utils.h"
-#include "dimq.h"
 
 using std::cout;
 using std::endl;
@@ -20,12 +18,30 @@ using std::string;
 using std::to_string;
 using std::vector;
 
+struct Constraint
+{
+    int line;
+    int index;
+    int length;
+    // range [rMin, rMax)
+    int rMin = 0;
+    int rMax = 0;
+    bool complete = false;
+};
+
+struct Section
+{
+    int start;
+    int length = 0;
+    int link;
+};
+
 class Game
 {
 private:
     // dimensions
-    int dim;
-    int dim2;
+    int dimSize;
+    int lines;
 
     // data
     Board board;
@@ -34,275 +50,90 @@ private:
     int maxConstraintNum = 0;
 
     vector<int> constraintSum;
+    vector<vector<Constraint>> constraintsVec;
     vector<regex> constraintsRegexLoose;
     vector<regex> constraintsRegexAbs;
-    vector<vector<int>> constraintsVec;
-    vector<vector<int>> constraintsVecOg;
-
-    // mutated lines
-    DIMQ mutated;
 
     // completion
     vector<bool> completion;
 
-    // ===== subroutine auxiliaries =====
-
-    // get first positive edge cell
-    pair<int, int> getFirstMarkedEdgePosition(string line)
+    // ===== auxiliary functions =====
+    // get minimum space required for constraints
+    int getMinSpace(vector<Constraint> *constraints, int start, int end)
     {
-        int first = -1;
-        int last = -1;
-
-        for (int i = 0; i < dim; ++i)
-        {
-            if (line[i] == '-')
-                break;
-            if (line[i] == '1')
-            {
-                first = i;
-                break;
-            }
-        }
-
-        for (int i = dim - 1; i >= 0; --i)
-        {
-            if (line[i] == '-')
-                break;
-            if (line[i] == '1')
-            {
-                last = i;
-                break;
-            }
-        }
-
-        return pair<int, int>(first, last);
-    }
-
-    // calculate the minimum space required for a set of constraints
-    int sumConstraintMinSpace(vector<int> *vector, int start = 0, int end = -1)
-    {
-        int size = vector->size();
-        if (end == -1)
-            end = size;
-
         int sum = 0;
         for (int i = start; i < end; ++i)
-            sum += (vector->at(i) + 1);
+            sum += (constraints->at(i).length + 1);
 
-        return sum - 1;
+        return sum;
     }
 
-    // get remaining continuous non-negative spaces
-    vector<pair<int, int>> getAllContinuousSpace(string line)
+    // fill in certain cells by constraint range
+    void fillCertain(int line)
     {
-        vector<pair<int, int>> spaces;
-        int size = 0;
-        int pointer = 0;
-        bool valid = false;
-        for (int i = 0; i < dim; ++i)
+        int size = constraintsVec[line].size();
+        vector<Constraint> *constraint = &constraintsVec[line];
+
+        for (int index = 0; index < size; ++index)
         {
-            char ch = line[i];
-            if (ch == '0')
-            {
-                if (size)
-                {
-                    if (valid)
-                        spaces.emplace_back(pair<int, int>(pointer, size));
-                    size = 0;
-                    pointer = i + 1;
-                    valid = false;
-                }
-                else
-                    pointer++;
-            }
-            else if (ch == '-')
-            {
-                valid = true;
-                size++;
-            }
-            else
-                size++;
-        }
+            Constraint c = constraint->at(index);
+            int start = c.rMax - c.length;
+            int end = c.rMin + c.length;
 
-        if (size)
-            spaces.emplace_back(pair<int, int>(pointer, size));
+            for (int i = start; i < end; ++i)
+                board.setCellByLine(line, i, true);
 
-        return spaces;
-    }
-
-    // ===== rules enforcers =====
-    void maxConstraintRule(int line, int start = 0, int length = -1, bool willComplete = false)
-    {
-        if (completion[line])
-            return;
-
-        if (length == -1)
-            length = dim;
-
-        vector<int> *constraint = &constraintsVec[line];
-        int conSize = constraint->size();
-
-        if (sumConstraintMinSpace(constraint) == length)
-        {
-            for (int i = 0; i < conSize; ++i)
-            {
-                int size = constraint->at(i);
-                for (int j = 0; j < size; ++j)
-                    if (board.setCellByLine(line, j + start, true))
-                        mutated.enqueue(line);
-
-                start += size + 1;
-            }
-
-            isComplete(line, willComplete);
+            if (end - start == c.length)
+                c.complete = true;
         }
     }
 
-    void constraintPaddingRule(int line, int start = 0, int length = -1)
+    // analyse line partitions
+    void analysePartitions(string target, vector<Section> *marked, vector<Section> *empty)
     {
-        if (completion[line])
-            return;
+        Section markedSection;
+        Section emptySection;
 
-        if (length == -1)
-            length = dim;
+        int emptyCount = 0;
 
-        vector<int> *constraint = &constraintsVec[line];
-        int conSize = constraint->size();
-
-        for (int i = 0; i < conSize; ++i)
+        for (int index = 0; index < dimSize; ++index)
         {
-            int pre = start + sumConstraintMinSpace(constraint, 0, i) + 1;
-            int remaining = length - pre - sumConstraintMinSpace(constraint, i + 1) - 1;
-            int gap = remaining - constraint->at(i);
-            int pointer = pre + gap;
-            for (int j = 0; j < remaining - gap * 2; ++j)
-                if (board.setCellByLine(line, j + pointer, true))
-                    mutated.enqueue(line);
-        }
+            char ch = target[index];
 
-        isComplete(line);
-    }
-
-    void edgeExtensionRule(int line, string target)
-    {
-        if (completion[line])
-            return;
-
-        vector<int> *constraint = &constraintsVec[line];
-        int conSize = constraint->size();
-
-        pair<int, int> positionPair = getFirstMarkedEdgePosition(target);
-
-        if (positionPair.first != -1)
-            for (int i = 1; i < constraint->at(0); ++i)
-                if (board.setCellByLine(line, positionPair.first + i, true))
-                    mutated.enqueue(line);
-
-        if (positionPair.second != -1)
-            for (int i = 1; i < constraint->at(conSize - 1); ++i)
-                if (board.setCellByLine(line, positionPair.second - i, true))
-                    mutated.enqueue(line);
-
-        isComplete(line);
-    }
-
-    void NegativePruningRule(int line, string target)
-    {
-        if (completion[line])
-            return;
-
-        for (int i = 0; i < dim; ++i)
-            if (target[i] == '-')
-            {
-                string temp = target;
-                temp[i] = '1';
-                if (!regex_match(temp, constraintsRegexLoose[line]))
-                    if (board.setCellByLine(line, i, false))
-                        mutated.enqueue(line);
-            }
-
-        isComplete(line);
-    }
-
-    void certainRemainingRule(int line, string target)
-    {
-        if (completion[line])
-            return;
-
-        int ones = 0;
-        vector<int> remainings;
-        for (int i = 0; i < dim; ++i)
-        {
-            if (target[i] == '-')
-                remainings.emplace_back(i);
-            else if (target[i] == '1')
-                ones++;
-        }
-
-        if (ones + remainings.size() == constraintSum[line])
-        {
-            for (int pos : remainings)
-                board.setCellByLine(line, pos, true);
-
-            isComplete(line, false, false);
-        }
-    }
-
-    // ===== completition checkers =====
-    void updateConstraints(int line)
-    {
-        if (completion[line])
-        {
-            constraintsVec[line] = vector<int>{};
-            return;
-        }
-
-        string target = board.read(line);
-        vector<int> constraint = cpy(constraintsVecOg[line]);
-
-        int counter = 0;
-        for (int i = 0; i < dim; ++i)
-        {
-            char ch = target[i];
             if (ch == '1')
-                counter++;
-            else if (ch == '0')
             {
-                if (counter)
-                {
-                    constraint.erase(constraint.begin());
-                    counter = 0;
-                }
+                if (markedSection.length == 0)
+                    markedSection.start = index;
+                markedSection.length++;
             }
-            else
-                break;
-        }
-        if (counter == constraint[0])
-            constraint.erase(constraint.begin());
-
-        counter = 0;
-        for (int i = dim - 1; i >= 0; --i)
-        {
-            char ch = target[i];
-            if (ch == '1')
-                counter++;
-            else if (ch == '0')
+            else if (markedSection.length > 0)
             {
-                if (counter)
-                {
-                    constraint.pop_back();
-                    counter = 0;
-                }
+                markedSection.link = emptyCount;
+                marked->emplace_back(markedSection);
+                markedSection = Section();
             }
-            else
-                break;
-        }
-        if (counter == constraint[constraint.size() - 1])
-            constraint.pop_back();
 
-        constraintsVec[line] = constraint;
+            if (ch != '0')
+            {
+                if (emptySection.length == 0)
+                    emptySection.start = index;
+                emptySection.length++;
+            }
+            else if (emptySection.length > 0)
+            {
+                empty->emplace_back(emptySection);
+                emptySection = Section();
+                emptyCount++;
+            }
+        }
+        if (markedSection.length > 0)
+            marked->emplace_back(markedSection);
+
+        if (emptySection.length > 0)
+            empty->emplace_back(emptySection);
     }
 
+    // check if line is complete
     bool isComplete(int line, bool checkingOverride = false, bool requireFill = true)
     {
         if (completion[line])
@@ -312,7 +143,7 @@ private:
         if (checkingOverride || regex_match(target, constraintsRegexAbs[line]))
         {
             if (requireFill)
-                for (int i = 0; i < dim; ++i)
+                for (int i = 0; i < dimSize; ++i)
                     board.setCellByLine(line, i, false);
 
             completion[line] = true;
@@ -327,94 +158,122 @@ public:
     Game(int dimIn)
     {
         // initialise board
-        dim = dimIn;
-        dim2 = dimIn * 2;
-        board = Board(dim);
+        dimSize = dimIn;
+        lines = dimIn * 2;
+        board = Board(dimSize);
 
         // initialise constraints data structures
-        constraintSum = vector<int>(dim2, 0);
+        constraintSum = vector<int>(lines, 0);
 
-        constraintsVec = vector<vector<int>>(dim2, vector<int>());
-        constraintsVecOg = vector<vector<int>>(dim2, vector<int>());
+        constraintsVec = vector<vector<Constraint>>(lines, vector<Constraint>());
 
         regex DEF("x");
-        constraintsRegexLoose = vector<regex>(dim2, DEF);
-        constraintsRegexAbs = vector<regex>(dim2, DEF);
-
-        // initialise mutated pruning queue
-        mutated = DIMQ(dim2);
+        constraintsRegexLoose = vector<regex>(lines, DEF);
+        constraintsRegexAbs = vector<regex>(lines, DEF);
 
         // initialise completion maintainers
-        completion = vector<bool>(dim2, false);
+        completion = vector<bool>(lines, false);
     }
 
     // set constraints
     void setRowConstraints(int index, vector<int> constraints)
     {
-        constraintsVec[index] = constraints;
-        constraintsVecOg[index] = cpy(constraints);
+        int size = constraints.size();
+
+        vector<Constraint> lineConstraints = vector<Constraint>(size, Constraint());
 
         string abs = "^[0|-]*";
         string loose = "^[0|-]*";
 
-        int size = constraints.size();
         for (int i = 0; i < size; ++i)
         {
-            constraintSum[index] += constraints[i];
-            abs += "1{" + to_string(constraints[i]) + (i + 1 < size ? "}[0|-]+" : "}");
+            int len = constraints[i];
+
+            // create constraint struct
+            lineConstraints[i].line = index;
+            lineConstraints[i].index = i;
+            lineConstraints[i].length = len;
+
+            // create matching regex
+            constraintSum[index] += len;
+            abs += "1{" + to_string(len) + (i + 1 < size ? "}[0|-]+" : "}");
             loose += "[1|";
             loose += (size > 1 ? "0|" : "");
-            loose += "-]{" + to_string(constraints[i]) + (i + 1 < size ? "}[0|-]+" : "}");
+            loose += "-]{" + to_string(len) + (i + 1 < size ? "}[0|-]+" : "}");
         }
 
         constraintsRegexAbs[index] = regex(abs + "[0|-]*$");
         constraintsRegexLoose[index] = regex(loose + "[0|-]*$");
 
+        // maintain max constraint number
         if (size > maxConstraintNum)
             maxConstraintNum = size;
+
+        // maintain constraint vector
+        constraintsVec[index] = lineConstraints;
     }
 
     void setColConstraints(int col, vector<int> constraints)
     {
-        setRowConstraints(col + dim, constraints);
+        setRowConstraints(col + dimSize, constraints);
     }
 
     // solve board
     void solve()
     {
-        bool complete = false;
-        int iteration = 0;
-
-        while (!complete && iteration < 1)
+        // maintain initial constraint range
+        for (int line = 0; line < lines; ++line)
         {
-            for (int line = 0; line < dim2; ++line)
+            int size = constraintsVec[line].size();
+            vector<Constraint> *constraint = &constraintsVec[line];
+
+            for (int index = 0; index < size; ++index)
             {
-                vector<pair<int, int>> spaces = getAllContinuousSpace(board.read(line));
-                if (spaces.size() == 1)
+                constraint->at(index).rMin = (getMinSpace(constraint, 0, index));
+                constraint->at(index).rMax = dimSize - getMinSpace(constraint, index + 1, size);
+            }
+        }
+
+        for (int line = 0; line < lines; ++line)
+            fillCertain(line);
+
+        for (int line = 0; line < lines; ++line)
+            isComplete(line);
+
+        for (int line = 0; line < lines; ++line)
+        {
+            if (completion[line])
+                continue;
+
+            string target = board.read(line);
+
+            vector<Section> markedSections;
+            vector<Section> emptySections;
+            analysePartitions(target, &markedSections, &emptySections);
+
+            int markedCount = markedSections.size();
+            int emptyCount = emptySections.size();
+
+            int constraintCount = constraintsVec[line].size();
+
+            if (markedCount == constraintCount)
+                for (int markedId = 0; markedId < markedCount; ++markedId)
                 {
-                    maxConstraintRule(line, spaces[0].first, spaces[0].second);
+                    Section marked = markedSections[markedId];
+                    int required = constraintsVec[line][markedId].length;
+                    int exMin = marked.start + marked.length - required;
+                    int exMax = marked.start + required;
 
-                    if (iteration == 0 && spaces[0].second > 2)
-                        constraintPaddingRule(line, spaces[0].first, spaces[0].second);
+                    Section empty = emptySections[marked.link];
+                    int eEnd = empty.start + empty.length;
+                    // if (eEnd - exMin > 0)
                 }
-            }
-
-            for (int line = 0; line < dim2; ++line)
+            else if (markedCount < constraintCount)
             {
-                string target = board.read(line);
-
-                edgeExtensionRule(line, target);
-                NegativePruningRule(line, target);
-                certainRemainingRule(line, target);
-
-                updateConstraints(line);
             }
-
-            complete = true;
-            for (bool lineComplete : completion)
-                complete *= lineComplete;
-                
-            iteration++;
+            else
+            {
+            }
         }
     }
 
@@ -426,7 +285,7 @@ public:
 
     int getDim()
     {
-        return dim;
+        return dimSize;
     }
 
     int getMaxConstraintNum()
@@ -434,9 +293,9 @@ public:
         return maxConstraintNum;
     }
 
-    vector<vector<int>> getConstraints()
+    vector<vector<Constraint>> getConstraints()
     {
-        return constraintsVecOg;
+        return constraintsVec;
     }
 };
 
