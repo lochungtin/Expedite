539e53bd5a73ee0188b8c3b13b26d05151ca0e99
lochungtin <lochungtin@gmail.com>
2022-10-21 17:09:16 +0100

    double pair prune reduction

--- Sudoku/solver.h
+++ Sudoku/solver.h
@@ -14,8 +14,8 @@ struct FrequencyTable
     int singleFrequency[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
     int singleLocation[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-    map<int, int> doublePairFrequency;
-    map<int, int> doublePairLocation;
+    int doubleFrequency[36] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    int doubleLocation[36] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 };
 
 struct SweepAnalysis
@@ -32,59 +32,105 @@ private:
     Game *game;
     stack<pair<Board, MetaState>> states;
 
-    void updateBoards(MetaState *metaState, int index, int value)
+    /**
+     * @brief Update metaState and game board
+     *
+     * @param metaState metaState pointer
+     * @param index     board index
+     * @param value     value to set
+     */
+    void updateBoard(MetaState *metaState, int index, int value)
     {
         game->setCell(index, value);
         metaState->setCell(index, value);
     }
 
-    SweepAnalysis frequencyAnalysis(int index, MetaState *metaState)
+    /**
+     * @brief Perform frequency analysis on possible values on the metaState
+     *
+     * @param metaState metaState pointer
+     * @param index     sweeper index
+     * @return SweepAnalysis (tuple of frequency tables)
+     */
+    void frequencyAnalysis(SweepAnalysis *sa, MetaState *metaState, int sweeper)
     {
-        // frequency table
-        SweepAnalysis sa;
-
-        // absolute values
-        int row = (index / 9) * 9;
-        int col = index % 9;
-
-        // scanning
+        // loop through all relative indices w.r.t. sweeper index
         for (int scan = 0; scan < 9; ++scan)
         {
             // calculate sweep indices
-            int rowIndex = row + scan;
-            int colIndex = col + (scan * 9);
-            int subIndex = subgrid2index(index, scan);
+            int rowIndex = sweeper * 9 + scan;
+            int colIndex = sweeper + (scan * 9);
+            int subIndex = subgrid2index(sweeper, scan);
 
+            // update
             for (int value = 0; value < 9; ++value)
             {
-                // update row single frequencies
                 if (!metaState->set[rowIndex] && metaState->possibles[rowIndex][value])
-                {
-                    sa.rFT.singleLocation[value] = rowIndex;
-                    sa.rFT.singleFrequency[value]++;
-                }
-                // update col single frequencies
+                    singleFrequencyUpdate(&sa->rFT, rowIndex, value);
                 if (!metaState->set[colIndex] && metaState->possibles[colIndex][value])
-                {
-                    sa.cFT.singleLocation[value] = colIndex;
-                    sa.cFT.singleFrequency[value]++;
-                }
-                // update sub single frequencies
+                    singleFrequencyUpdate(&sa->cFT, colIndex, value);
                 if (!metaState->set[subIndex] && metaState->possibles[subIndex][value])
-                {
-                    sa.sFT.singleLocation[value] = subIndex;
-                    sa.sFT.singleFrequency[value]++;
-                }
+                    singleFrequencyUpdate(&sa->sFT, subIndex, value);
             }
 
             // update row double pair frequencies
-            if (metaState->getPossibleSize(rowIndex) == 2)
+            if (!metaState->set[rowIndex] && metaState->getPossibleSize(rowIndex) == 2)
             {
+                // cout << scan << " r " << rowIndex << endl;
+                doubleFrequencyUpdate(metaState, &sa->rFT, rowIndex, scan);
+            }
+
+            if (!metaState->set[colIndex] && metaState->getPossibleSize(colIndex) == 2)
+            {
+                // cout << scan << " c " << colIndex << endl;
+                doubleFrequencyUpdate(metaState, &sa->cFT, colIndex, scan);
+            }
+
+            if (!metaState->set[subIndex] && metaState->getPossibleSize(subIndex) == 2)
+            {
+                // cout << scan << " s " << subIndex << endl;
+                doubleFrequencyUpdate(metaState, &sa->sFT, subIndex, scan);
             }
         }
-
-        return sa;
     };
+
+    /**
+     * @brief Update frequency table with single possible data
+     *
+     * @param ft    frequency table pointer
+     * @param value possible value to update
+     * @param index location of possible value
+     */
+    void singleFrequencyUpdate(FrequencyTable *ft, int index, int value)
+    {
+        ft->singleLocation[value] = index;
+        ft->singleFrequency[value]++;
+    }
+
+    /**
+     * @brief Update frequency table with double possible value data
+     *
+     * @param metaState metaState pointer
+     * @param ft        frequency table pointer
+     * @param index     board index
+     * @param scan      relative index
+     */
+    void doubleFrequencyUpdate(MetaState *metaState, FrequencyTable *ft, int index, int scan)
+    {
+        int pair = 0;
+        int shift = 10;
+        for (int i = 0; i < 9; ++i)
+            if (metaState->possibles[index][i])
+            {
+                pair += i * shift;
+                shift /= 10;
+            }
+
+        int sign = pair2signature(pair);
+
+        ft->doubleLocation[sign] += (10 - 9 * ft->doubleFrequency[sign]) * scan;
+        ft->doubleFrequency[sign]++;
+    }
 
 public:
     Solver(Game *game) : game(game)
@@ -92,6 +138,11 @@ public:
         states.push(pair<Board, MetaState>{*game->getBoard(), MetaState()});
     }
 
+    /**
+     * @brief Run solver on board
+     *
+     * @return iterations taken to solve
+     */
     int run()
     {
         Board *board = &states.top().first;
@@ -109,29 +160,99 @@ public:
         while (metaState->incomplete() && iterations < 20)
         {
             iterations++;
+
+            // frequency analysis for possible value pruning
             for (int sweeper = 0; sweeper < 9; ++sweeper)
             {
-                SweepAnalysis sa = frequencyAnalysis(sweeper, metaState);
+                SweepAnalysis sa;
+                frequencyAnalysis(&sa, metaState, sweeper);
+
+                // prune possible values from cells with double pair rule
+                for (int sign = 0; sign < 36; ++sign)
+                {
+                    if (sa.rFT.doubleFrequency[sign] == 2)
+                    {
+                        int location = sa.rFT.doubleLocation[sign];
+                        int l0 = location / 10;
+                        int l1 = location % 10;
+
+                        int pair = signature2pair(sign);
+                        int p0 = pair / 10;
+                        int p1 = pair % 10;
+
+                        for (int index = 0; index < 9; ++index)
+                        {
+                            int rowIndex = sweeper * 9 + index;
+                            if (!metaState->set[rowIndex] && index != l0 && index != l1)
+                            {
+                                metaState->possibles[rowIndex][p0] = false;
+                                metaState->possibles[rowIndex][p1] = false;
+                            }
+                        }
+                    }
+
+                    if (sa.cFT.doubleFrequency[sign] == 2)
+                    {
+                        int location = sa.cFT.doubleLocation[sign];
+                        int l0 = location / 10;
+                        int l1 = location % 10;
+
+                        int pair = signature2pair(sign);
+                        int p0 = pair / 10;
+                        int p1 = pair % 10;
+
+                        for (int index = 0; index < 9; ++index)
+                        {
+                            int colIndex = sweeper + index * 9;
+                            if (!metaState->set[colIndex] && index != l0 && index != l1)
+                            {
+                                metaState->possibles[colIndex][p0] = false;
+                                metaState->possibles[colIndex][p1] = false;
+                            }
+                        }
+                    }
+
+                    if (sa.sFT.doubleFrequency[sign] == 2)
+                    {
+                        int location = sa.sFT.doubleLocation[sign];
+                        int l0 = location / 10;
+                        int l1 = location % 10;
+
+                        int pair = signature2pair(sign);
+                        int p0 = pair / 10;
+                        int p1 = pair % 10;
+
+                        for (int index = 0; index < 9; ++index)
+                        {
+                            int subIndex = subgrid2index(sweeper, index);
+                            if (!metaState->set[subIndex] && index != l0 && index != l1)
+                            {
+                                metaState->possibles[subIndex][p0] = false;
+                                metaState->possibles[subIndex][p1] = false;
+                            }
+                        }
+                    }
+                }
 
                 // set cells to value with freq 1
                 for (int value = 0; value < 9; ++value)
                 {
                     if (sa.rFT.singleFrequency[value] == 1)
-                        updateBoards(metaState, sa.rFT.singleLocation[value], value);
+                        updateBoard(metaState, sa.rFT.singleLocation[value], value);
                     if (sa.cFT.singleFrequency[value] == 1)
-                        updateBoards(metaState, sa.cFT.singleLocation[value], value);
+                        updateBoard(metaState, sa.cFT.singleLocation[value], value);
                     if (sa.sFT.singleFrequency[value] == 1)
-                        updateBoards(metaState, sa.sFT.singleLocation[value], value);
+                        updateBoard(metaState, sa.sFT.singleLocation[value], value);
                 }
             }
 
             // set cells with only one possible value left
             for (int index = 0; index < 81; ++index)
                 if (!metaState->set[index] && metaState->getPossibleSize(index) == 1)
-                    updateBoards(metaState, index, metaState->getSingle(index));
+                    updateBoard(metaState, index, metaState->getSingle(index));
         }
 
-        metaState->listIncomplete();
+        // metaState->listIncomplete();
 
         return iterations;
     }
