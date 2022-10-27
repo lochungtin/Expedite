7efe48d6af848ae7ff9e2860ec42d0ab89e7c86e
lochungtin <lochungtin@gmail.com>
2022-10-27 11:22:29 +0100

    refactor

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let settingsOpen = false;
 let infoOpen = false;
 
-const toggleSettings = open => {
-    settingsOpen = open;
-    document.getElementById('settings').style.width = settingsOpen ? '100%' : '0%';
-}
+const toggleSettings = (open) => {
+	settingsOpen = open;
+	document.getElementById('settings').style.width = settingsOpen ? '100%' : '0%';
+};
 
-const toggleInfo = open => {
-    infoOpen = open;
-    document.getElementById('info').style.width = infoOpen ? '100%' : '0%';
-}
+const toggleInfo = (open) => {
+	infoOpen = open;
+	document.getElementById('info').style.width = infoOpen ? '100%' : '0%';
+};
