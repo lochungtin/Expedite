9e94c06ed6dab3c908b50674ffb8bb1bbc0e0aac
lochungtin <lochungtin@gmail.com>
2023-10-21 17:59:29 +0100

    nightly

--- solvers/sudoku.py
+++ solvers/sudoku.py
@@ -1,3 +1,5 @@
+from copy import deepcopy as dc
+
 from solvers.__solver import __Solver
 
 
@@ -6,6 +8,8 @@ class Solver(__Solver):
         super().__init__(game)
         self.p = [[[True for _ in range(9)] for _ in range(9)] for _ in range(9)]
         self.s = [[False for _ in range(9)] for _ in range(9)]
+        self.t = [[0 for _ in range(9)] for _ in range(9)]
+        self.g = []
 
     def run(self):
         for r in range(9):
@@ -13,15 +17,51 @@ class Solver(__Solver):
                 if self.b[r][c] != " ":
                     self.__set_board(r, c, int(self.b[r][c]) - 1)
 
-        iteration = -1
-        while not self.__validation() and iteration < 10:
-            iteration += 1
+        unsolved = True
+        iterations = 0
+        while unsolved:
+            iterations += 1
+            print(iterations)
+            print([g[3] for g in self.g])
+            if not self.__propagate():
+                if len(self.g):
+                    self.p, self.s, self.t, (r, c, n) = self.g.pop()
+                    self.__set_board(r, c, n)
+                else:
+                    return None
+
+            if self.__validation():
+                for r, row in enumerate(self.t):
+                    for c, cell in enumerate(row):
+                        self.b[r][c] = cell
+                        unsolved = False
+            else:
+                r, c, n = sorted(
+                    [
+                        (r, c, self.__poss_val(cell))
+                        for r, row in enumerate(self.p)
+                        for c, cell in enumerate(row)
+                        if self.__poss_len(cell) > 1
+                    ],
+                    key=lambda x: len(x[2]),
+                )[0]
+                n = list(reversed(n))
+                for i in range(1, len(n)):
+                    self.g.append((dc(self.p), dc(self.s), dc(self.t), (r, c, n[i])))
+
+                self.__set_board(r, c, n[0])
+
+        return iterations
+
+    def __propagate(self):
+        changed = True
+        while changed:
+            changed = False
             for i in range(9):
                 idxposs = [
                     list(zip(idx, [self.p[r][c] for (r, c) in idx]))
                     for idx in [self.__r_idx(i), self.__c_idx(i), self.__s_idx(i)]
                 ]
-
                 rSpans = dict([(i, set()) for i in range(9)])
                 cSpans = dict([(i, set()) for i in range(9)])
                 for (r, c), poss in idxposs[2]:
@@ -32,13 +72,18 @@ class Solver(__Solver):
                                 cSpans[p].add(c)
                 for c, v in [(list(l)[0], i) for i, l in cSpans.items() if len(l) == 1]:
                     for r in range(9):
-                        if r // 3 != i // 3 and not self.s[r][c]:
+                        if r // 3 != i // 3 and not self.s[r][c] and self.p[r][c][v]:
                             self.p[r][c][v] = False
+                            if self.__poss_len(self.p[r][c]) == 0:
+                                return False
+                            changed = True
                 for r, v in [(list(l)[0], i) for i, l in rSpans.items() if len(l) == 1]:
                     for c in range(9):
-                        if c // 3 != i % 3 and not self.s[r][c]:
+                        if c // 3 != i % 3 and not self.s[r][c] and self.p[r][c][v]:
                             self.p[r][c][v] = False
-
+                            if self.__poss_len(self.p[r][c]) == 0:
+                                return False
+                            changed = True
                 for pair in idxposs:
                     len2 = list(filter(lambda x: self.__poss_len(x[1]) == 2, pair))
                     for a, (l0, p0) in enumerate(len2):
@@ -47,8 +92,11 @@ class Solver(__Solver):
                                 for (r, c), _ in pair:
                                     if (r, c) != l0 and (r, c) != l1:
                                         for v in self.__poss_val(self.p[l0[0]][l0[1]]):
-                                            self.p[r][c][v] = False
-
+                                            if self.p[r][c][v]:
+                                                self.p[r][c][v] = False
+                                                if self.__poss_len(self.p[r][c]) == 0:
+                                                    return False
+                                                changed = True
                 for pair in idxposs:
                     counts = dict([(i, []) for i in range(9)])
                     for (r, c), poss in pair:
@@ -59,32 +107,37 @@ class Solver(__Solver):
                     for v, l in counts.items():
                         if len(l) == 1:
                             self.__set_board(l[0][0], l[0][1], v)
-
-            self.__fillSingle()
-        self.__fillSingle()
-
-        return iteration
+                            changed = True
+            if self.__fillSingle():
+                changed = True
+        if self.__fillSingle():
+            changed = True
+        return True
 
     def __set_board(self, r, c, v):
         if self.s[r][c]:
-            return
+            return False
         idx = self.__r_idx(r) + self.__c_idx(c) + self.__s_idx((r // 3) * 3 + (c // 3))
-        for (i, j) in list(set(idx)):
+        for i, j in list(set(idx)):
             self.p[i][j][v] = False
-        self.p[r][c][v], self.s[r][c], self.b[r][c] = True, True, str(v + 1)
+        self.p[r][c][v], self.s[r][c], self.t[r][c] = True, True, v + 1
+        return True
 
     def __validation(self):
         for i in range(9):
             s = [self.__r_idx(i), self.__c_idx(i), self.__s_idx(i)]
-            if any(map(lambda x: len(set([self.b[r][c] for (r, c) in x])) != 9, s)):
+            if any(map(lambda x: len(set([self.t[r][c] for (r, c) in x])) != 9, s)):
                 return False
         return True
 
     def __fillSingle(self):
+        changed = False
         for i in range(9):
             for j in range(9):
                 if not self.s[i][j] and self.__poss_len(self.p[i][j]) == 1:
-                    self.__set_board(i, j, self.__poss_val(self.p[i][j])[0])
+                    if self.__set_board(i, j, self.__poss_val(self.p[i][j])[0]):
+                        changed = True
+        return changed
 
     def __poss_len(self, p):
         return len(list(filter(lambda x: x, p)))
