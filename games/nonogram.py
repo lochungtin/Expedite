f91ce790eabd640c9e5ae32114498a1d2e925bff
lochungtin <lochungtin@gmail.com>
2023-10-25 01:23:24 +0100

    nightly

--- games/nonogram.py
+++ games/nonogram.py
@@ -1,16 +1,37 @@
+import re
 from games.__game import __Game
-
 
 class Game(__Game):
     def __init__(self, lines):
         super().__init__(True)
         self.size = int(lines[0])
         self.board = [["-" for _ in range(self.size)] for _ in range(self.size)]
-        self.rCon = [c.strip().split(" ") for c in lines[1 : self.size + 1]]
-        self.cCon = [c.strip().split(" ") for c in lines[self.size + 1 :]]
+        self.rCon = [list(map(int, c.strip().split(" "))) for c in lines[1 : self.size + 1]]
+        self.cCon = [list(map(int, c.strip().split(" "))) for c in lines[self.size + 1 :]]
+        self.reCon = list(map(self.__toRe, self.rCon + self.cCon))
+        self.comp = [0 for _ in range(self.size * 2)]
+        
+    def __toRe(self, c):
+        mid = "[0|-]+".join(["1{" + str(x) + "}" for x in c])
+        return f"[0|-]*{mid}[0|-]*"
+    
+    def line(self, l):
+        if l >= self.size:
+            return [self.board[i][l % self.size] for i in range(self.size)]
+        return [self.board[l][i] for i in range(self.size)]
 
     def setCell(self, row, col, value):
         if self.board[row][col] == "-":
             self.board[row][col] = str(value * 1)
+            if re.match(self.reCon[row], "".join(self.line(row))):
+                for i in range(self.size):
+                    if self.board[row][i] == "-":
+                        self.board[row][i] = "0"
+                self.comp[row] = 1
+            if re.match(self.reCon[self.size + col], "".join(self.line(self.size + col))):
+                for i in range(self.size):
+                    if self.board[i][col] == "-":
+                        self.board[i][col] = "0"
+                self.comp[self.size + col] = 1
             return True
         return False
--- solvers/nonogram.py
+++ solvers/nonogram.py
@@ -4,26 +4,60 @@ from solvers.__solver import __Solver
 class Solver(__Solver):
     def __init__(self, game) -> None:
         super().__init__(game)
-        self.c = [list(map(int, c)) for c in (game.rCon + game.cCon)]
-        self.e = [[(0, self.d)] for _ in range(self.d * 2)]
+        self.c = game.rCon + game.cCon
 
-    def lFill(self, l, i, v):
+    def run(self):
+        iteration = -1
+        while iteration < 1:
+            iteration += 1
+            for l, c in enumerate(self.c):
+                if not self.g.comp[l]:
+                    self.__overlapFill(l, c, self.__fs(l))
+                    self.__condSumFill(l, c, self.__fs(l))
+                    self.__extFill(l, c)
+                    self.__sideFill(l, c)
+        return iteration
+
+    def __lFill(self, l, i, v=1):
         if l >= self.d:
             l, i = i, l % self.d
         self.g.setCell(l, i, v)
 
-    def lRangeFill(self, l, v, s, n):
-        [self.lFill(l, i, v) for i in range(s, s + n)]
+    def __lRangeFill(self, l, s, n, v=1):
+        [self.__lFill(l, i, v) for i in range(max(s, 0), min(s + n, self.d))]
 
-    def halfWidthMinFill(self, l, e, n):
-        t = n * 2 - e[1]
-        self.lRangeFill(l, 1, n - t + e[0], t)
+    def __fs(self, l):
+        a = list(map(lambda x: x == "0", self.g.line(l)))
+        return a.index(False), self.d - list(reversed(a)).index(False)
 
-    def conMaxFill(self, l, e, c):
-        if sum(c) + len(c) == e[1] + 1:
-            loc = list(map(int, " 1 ".join(map(str, c)).split(" ")))
-            for i, s in enumerate(loc):
-                self.lRangeFill(l, (i + 1) % 2, e[0] + sum(loc[0:i]), s)
+    def __overlapFill(self, l, c, f):
+        if len(c) == 1:
+            self.__lRangeFill(l, f[1] - c[0], c[0] * 2 - f[1] + f[0])
 
-    def run(self):
-        self.conMaxFill(0, (0, 10), [1, 2, 2, 2])
+    def __condSumFill(self, l, c, f):
+        if len(c) + sum(c) - 1 == f[1] - f[0]:
+            idx = f[0]
+            for p in c:
+                self.__lRangeFill(l, idx, p)
+                idx += p + 1
+        
+    def __extFill(self, l, c):
+        if len(c) == 1:
+            a = self.g.line(l)
+            if "1" in a:
+                le1, ri1 = a.index("1"), self.d - list(reversed(a)).index("1") - 1
+                self.__lRangeFill(l, le1, ri1 - le1)
+                le0, ri0 = 0, 10
+                if "0" in a[0:le1]:
+                    le0 = le1 - list(reversed(a[0:le1])).index("0") - 1
+                if "0" in a[ri1:10]:
+                    ri0 = ri1 + a[ri1:10].index("0")
+                self.__lRangeFill(l, ri1 + 1, c[0] - ri1 + le0)
+                self.__lRangeFill(l, ri0 - le1, le1 + c[0] - ri0)
+
+    def __sideFill(self, l, c):
+        a = self.g.line(l)
+        if a[0] == "1":
+            self.__lRangeFill(l, 0, c[0])
+        if a[-1] == "1":
+            self.__lRangeFill(l, self.d - c[-1], c[-1])
