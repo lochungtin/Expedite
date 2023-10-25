6e7e95d37252fa531d2c4e23d9afcbd0b54162fd
lochungtin <lochungtin@gmail.com>
2023-10-25 03:07:22 +0100

    update

--- games/2048.py
+++ games/2048.py
@@ -6,4 +6,4 @@ class Game(__Game):
         super().__init__(True)
         self.size = 4
         self._m = dict([(0, " ")] + [(2 ** (i + 1), chr(i + 97)) for i in range(17)])
-        self.board = [[0 for _ in range(4)] for _ in range(4)]
+        self.board = [[2 ** (i * 4 + j + 1) for i in range(4)] for j in range(4)]
