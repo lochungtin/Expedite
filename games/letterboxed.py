cf3e267c9d00fa9d8ee295a08260ce8d969d3a09
lochungtin <lochungtin@gmail.com>
2023-11-27 22:02:01 +0000

    update

--- games/letterboxed.py
+++ games/letterboxed.py
@@ -3,9 +3,9 @@ from games.__game import __Game
 
 class Game(__Game):
     def __init__(self, lines):
-        super().__init__(True)
+        super().__init__()
         self.sides = [line.strip().split(" ") for line in lines]
-        self.board = []
+        self.board = [f"[{line.strip()}]" for line in lines]
 
     def __repr__(self):
         return "-".join(self.board)
